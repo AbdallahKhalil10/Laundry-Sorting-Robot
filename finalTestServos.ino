ad5634f70f6c3e07c5826c04fa3e66b4b2c81aa9
AbdallahKhalil10 <khalilabdalla829@gmail.com>
2024-05-10 14:33:55 +0300

    code with color sensor and servos (missing white & black)

--- finalTestServos.ino
+++ finalTestServos.ino
@@ -7,12 +7,13 @@ Servo armServo;
 Servo gripServo;
 Servo rotateGripServo;
 ServoSmooth servoSmooth;
+ServoSmooth servoSmooth2;
 
 
 
 // Define color sensor pins
-/*#define S0 4
-#define S1 5
+#define S0 4
+#define S1 13
 #define S2 2
 #define S3 7
 #define sensorOut 8
@@ -71,7 +72,7 @@ int getBluePW() {
   PW = pulseIn(sensorOut, LOW);
   // Return the value
   return PW;
-}*/
+}
 
 void moveSmoothServo(int upAndDownPosition, int delayTime = 0) {
   servoSmooth.setTargetDeg(upAndDownPosition);
@@ -81,25 +82,32 @@ void moveSmoothServo(int upAndDownPosition, int delayTime = 0) {
   delay(delayTime);
 }
 
+void moveSmoothServoTwo(int armPosition, int delayTime = 0) {
+  servoSmooth2.setTargetDeg(armPosition);
+  while (!servoSmooth2.tick())
+    ;
+  Serial.println("Servo 20K 2 moved to position: " + (String)armPosition);
+  delay(delayTime);
+}
+
 void moveAllServos(int basePosition, int armPosition, int gripPosition, int rotateGripPosition, int upAndDownPosition, int delayTime = 0) {
 
   baseServo.write(basePosition);
   Serial.print("Base moved ");
   delay(delayTime);
 
-  moveSmoothServo(upAndDownPosition, 2000);
+  moveSmoothServoTwo(armPosition, delayTime);
   Serial.print("Servo 20kg moved ");
 
-  armServo.write(armPosition);
-  Serial.print("Arm moved ");
-  delay(delayTime);
-
-  rotateGripServo.write(rotateGripPosition);
-  Serial.print("Rotate moved ");
-
-  // gripServo.write(gripPosition);
-  // Serial.print("Grip moved ");
+  // rotateGripServo.write(rotateGripPosition);
+  // Serial.print("Rotate moved ");
   // delay(delayTime);
+
+  gripServo.write(gripPosition);
+  Serial.print("Grip moved ");
+
+  moveSmoothServo(upAndDownPosition, delayTime);
+  Serial.print("Servo 20kg moved ");
 }
 
 
@@ -108,12 +116,6 @@ void moveAllServos(int basePosition, int armPosition, int gripPosition, int rota
 void moveBase(int basePosition, int delayTime = 0) {
   baseServo.write(basePosition);
   Serial.print("Base moved  ");
-  delay(delayTime);
-}
-
-void moveArm(int armPosition, int delayTime = 0) {
-  armServo.write(armPosition);
-  Serial.print("Arm moved  ");
   delay(delayTime);
 }
 
@@ -134,42 +136,72 @@ void grip(int gripPosition, int delayTime = 0) {
 void goToRed() {
   Serial.println();
   Serial.println("Red detected  ");
-  int armPositionPick = 120;
-  moveArm(armPositionPick, 2000);
-  int upAndDownPosition = 30;
-  moveSmoothServo(upAndDownPosition, 2000);
+  int armPositionPick = 60;
+  moveSmoothServoTwo(armPositionPick, 2000);
   int basePositionPick = 20;
   moveBase(basePositionPick, 2000);
-  armPositionPick = 150;
-  moveArm(armPositionPick, 2000);
+  armPositionPick = 0;
+  moveSmoothServoTwo(armPositionPick, 1000);
+  int upAndDownPosition = 60;
+  moveSmoothServo(upAndDownPosition, 1000);
 }
 
-/*void goToGreen() {
+void goToGreen() {
   Serial.println();
   Serial.println("Green detected  ");
-  int armPositionPick = 90;
-  moveArm(armPositionPick, 2000);
-  int basePositionPick = 170;
+  int armPositionPick = 60;
+  moveSmoothServoTwo(armPositionPick, 2000);
+  int basePositionPick = 50;
   moveBase(basePositionPick, 2000);
-  armPositionPick = 50;
-  moveArm(armPositionPick, 2000);
+  armPositionPick = 0;
+  moveSmoothServoTwo(armPositionPick, 1000);
+  int upAndDownPosition = 60;
+  moveSmoothServo(upAndDownPosition, 1000);
 }
 
 void goToBlue() {
   Serial.println();
-  Serial.println("Blue detected  ");
-  int armPositionPick = 90;
-  moveArm(armPositionPick, 2000);
-  int basePositionPick = 90;
+  int armPositionPick = 60;
+  moveSmoothServoTwo(armPositionPick, 2000);
+  int basePositionPick = 100;
   moveBase(basePositionPick, 2000);
-  armPositionPick = 50;
-  moveArm(armPositionPick, 2000);
-}*/
+  armPositionPick = 0;
+  moveSmoothServoTwo(armPositionPick, 1000);
+  int upAndDownPosition = 60;
+  moveSmoothServo(upAndDownPosition, 1000);
+}
+
+void goToWhite() {
+  int armPositionPick = 60;
+  moveSmoothServoTwo(armPositionPick, 2000);
+  int basePositionPick = 40;
+  moveBase(basePositionPick, 2000);
+  armPositionPick = 0;
+  moveSmoothServoTwo(armPositionPick, 1000);
+  int upAndDownPosition = 60;
+  moveSmoothServo(upAndDownPosition, 1000);
+}
+void goToBlack() {
+  int armPositionPick = 60;
+  moveSmoothServoTwo(armPositionPick, 2000);
+  int basePositionPick = 70;
+  moveBase(basePositionPick, 2000);
+  armPositionPick = 0;
+  moveSmoothServoTwo(armPositionPick, 1000);
+  int upAndDownPosition = 60;
+  moveSmoothServo(upAndDownPosition, 1000);
+}
 
 
 // Function to detect the color & move the SERVOs to the required location
-/*void getColor() {
+void getColor() {
+
+  int upAndDownPosition = 0;
+  moveSmoothServo(upAndDownPosition, 2000);
+
   // Read Red Pulse Width
+  int whiteCounter = 0;
+  int blackCounter = 0;
   int redCounter = 0;
   int blueCounter = 0;
   int greenCounter = 0;
@@ -190,6 +222,11 @@ void goToBlue() {
       greenCounter++;
     else if ((bluePW < redPW && bluePW < greenPW))  //Detect Blue Color
       blueCounter++;
+    // TO DO: CALIBRATION OF WHITE & BLACK COLORS
+    else if ((bluePW < redPW && bluePW < greenPW))  //Detect Blue Color
+      whiteCounter++;
+    else if ((bluePW < redPW && bluePW < greenPW))  //Detect Blue Color
+      blackCounter++;
     Serial.println("///////////////////////////////////////////");
     Serial.print("Loop number: ");
     Serial.println(i + 1);
@@ -202,63 +239,75 @@ void goToBlue() {
     Serial.println("///////////////////////////////////////////");
   }
 
-  if (redCounter > greenCounter && redCounter > blueCounter)
+  if (redCounter > greenCounter && redCounter > blueCounter && redCounter > whiteCounter && redCounter > blackCounter)
     goToRed();
-  else if (greenCounter > redCounter && greenCounter > blueCounter)
+  else if (greenCounter > redCounter && greenCounter > blueCounter && greenCounter > whiteCounter && greenCounter > blackCounter)
     goToGreen();
-  else if (blueCounter > redCounter && blueCounter > greenCounter)
+  else if (blueCounter > redCounter && blueCounter > greenCounter && blueCounter > whiteCounter && blueCounter > blackCounter)
     goToBlue();
-}*/
+  else if (whiteCounter > redCounter && whiteCounter > greenCounter && whiteCounter > blueCounter && whiteCounter > blackCounter)
+    goToWhite();
+  else if (blackCounter > redCounter && blackCounter > greenCounter && blackCounter > blueCounter && blackCounter > whiteCounter)
+    goToBlack();
+}
 
 
 // Function to attach servos to pins
 void attachServos() {
   baseServo.attach(9);         // Attach baseServo to pin 9
-  armServo.attach(10);         // Attach armServo to pin 10
   gripServo.attach(6);         // Attach gripServo to pin 11
   rotateGripServo.attach(11);  // Attach rotateGripServo to pin 6
 }
 
 void attachServos20K() {
   servoSmooth.attach(5, 400, 2400);  // Attach servo to pin 5
-  servoSmooth.setAutoDetach(true);
+  servoSmooth.setAutoDetach(false);
   servoSmooth.setSpeed(100);  // Set speed value
   servoSmooth.setAccel(0);
+
+  servoSmooth2.attach(10, 400, 2400);  // Attach servo to pin 5
+  servoSmooth2.setAutoDetach(false);
+  servoSmooth2.setSpeed(100);  // Set speed value
+  servoSmooth2.setAccel(0);
 }
+
 
 // Function to initialize servo positions
 void initializePositions() {
   int basePositionInitial = 50;
-  int armPositionInitial = 90;
+  int armPositionInitial = 0;
   int gripPositionInitial = 30;
   int rotateGripPositionInitial = 90;
   int upAndDownPosition = 0;
-  moveAllServos(basePositionInitial, armPositionInitial, gripPositionInitial, rotateGripPositionInitial, upAndDownPosition, 2000);
+  moveAllServos(basePositionInitial, armPositionInitial, gripPositionInitial, rotateGripPositionInitial, upAndDownPosition);
 }
 
 void goToLaundry() {
   int basePositionLaundry = 0;
-  int armPositionLaundry = 150;
+  int armPositionLaundry = 0;
   int gripPositionLaundry = 30;
   int rotateGripPositionLaundry = 90;
-  int upAndDownPosition = 100;
+  int upAndDownPosition = 60;
   moveAllServos(basePositionLaundry, armPositionLaundry, gripPositionLaundry, rotateGripPositionLaundry, upAndDownPosition, 1000);
 }
 
 void pickAndDetectAndRelease() {
 
   int gripPositionPick = 90;
-  grip(gripPositionPick, 2000);
-  goToRed();
-  //getColor();
+  grip(gripPositionPick, 1000);
+  //goToRed();
+  getColor();
   gripPositionPick = 30;
-  grip(gripPositionPick, 3000);
+  grip(gripPositionPick, 2000);
+  int upAndDownPosition = 0;
+  moveSmoothServo(upAndDownPosition, 2000);
 }
 
 void makeLaundry() {
   goToLaundry();
-  delay(2000);
+  delay(1000);
   pickAndDetectAndRelease();
+  initializePositions();
 }
 
 
@@ -271,11 +320,8 @@ void setup() {
   initializePositions();
   delay(3000);
   makeLaundry();
-  //delay(2000);
 }
 
 void loop() {
   // // Main code will go here
-  // makeLaundry();
-  // delay(2000);
 }
